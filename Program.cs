d096a4497db2c0d6309f3fcfe09a5aba4310d165
zekha <zeke.hasametaj@yahoo.com>
2022-11-13 22:23:11 +0100

    Hallo

--- Program.cs
+++ Program.cs
@@ -5,6 +5,78 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            // Console.WriteLine("Hello World!");
+            /*
+            Console.WriteLine("Congrats!");
+            Console.WriteLine(" ");
+            Console.WriteLine("First line of code");
+
+            Console.WriteLine("This is the first line.");
+            Console.WriteLine(" ");
+            Console.WriteLine("This is the second line.");
+
+            2 Back slashes escapen dem Steuerzeichen also \n = Zeilen umbruch ausgabe, \\n = wird als zeichen ausgegeben also \n
+          
+            Console.WriteLine("This is the first line.");
+            Console.WriteLine("This is the ");
+            Console.WriteLine("second line.");
+
+            Console.WriteLine('b');
+            Console.WriteLine(123);
+            Console.WriteLine(12.30m);
+            Console.WriteLine(true);
+            Console.WriteLine(false);
+
+            Console.WriteLine("123");
+            Console.WriteLine(123);
+
+            Console.WriteLine("true");
+            Console.WriteLine(true);
+
+            string firstName;
+
+            char userOption;
+
+            int gameScore;
+
+            decimal particlesPerMillion;
+
+            bool processedCustomer;
+             
+            string name = "Bob";
+            int messages = 3;
+            decimal temperature = 34.4m;
+             Console.WriteLine($"Hello {name} you got {messages} messages and its {temperature} celsius");
+
+            Console.WriteLine("Hello\nWorld!");
+            Console.WriteLine("Hello\tWorld!");
+            Console.WriteLine("Hello \"World\"!");
+
+            string firstName = "Bob";
+            string message = "Hello " + firstName;
+            Console.WriteLine(message);
+            
+
+            string projectName = "ACME";
+            string russianMessage = " \u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
+
+            Console.WriteLine($"View English output:\n\t\tc:\\Exercise\\{projectName}\\ru-RU\\data.txt\n\n");
+            Console.WriteLine(russianMessage + $":\n\t\tc:\\Exercise\\{projectName}\\ru-RU\\data.txt");
+
+
+            decimal a = 0.1m;
+            decimal b = 0.2m;
+
+            if (a+b == 0.3m)
+            {
+                Console.WriteLine("richtig");
+            } else
+            {
+                Console.WriteLine("falsch");
+            }
+           */
+
+
         }
     }
 }
